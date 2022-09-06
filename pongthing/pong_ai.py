63dfd876812bf68312eed1bbefb06f3272be8632
penneShells <61021287+penneShells@users.noreply.github.com>
2022-09-06 07:32:14 +0100

    Update pong_ai.py
    Buggy as hell and doesn't work yet but I'm on it

--- pongthing/pong_ai.py
+++ pongthing/pong_ai.py
@@ -21,18 +21,41 @@ pygame.display.set_caption("Pong Time")
 def main(genomes, config):
 
     paddles = []
-    
+    nets = []
+    ge = []
+    all_sprites_list = pygame.sprite.Group()
+    counter = []
+    balls = []
+    for genome_id, g in genomes:
+        g.fitness = 0
+        net = neat.nn.FeedForwardNetwork.create(g, config)
+        nets.append(net)
+        paddles.append(Paddle(white, 10, 100))
+        print("eep")
+        ge.append(g)
+       
+    for paddle in paddles:
+        paddle.rect.x = 670
+        paddle.rect.y = 200
+        all_sprites_list.add(paddle)
+        count = 0
+        counter.append(count)
+   
     wall = Paddle(white, 10, 10000)
     wall.rect.x = 20
     wall.rect.y = 0
-     
+    '''
     paddleB = Paddle(white, 10, 100)
     paddleB.rect.x = 670
     paddleB.rect.y = 200
-
-    ball = Ball(white,10,10)
-    ball.rect.x = 345
-    ball.rect.y = 195
+'''
+    for paddle in paddles:
+        ball = Ball(white,10,10)
+        ball.rect.x = 345
+        ball.rect.y = 195
+        balls.append(ball)
+        all_sprites_list.add(ball)
+        print("e")
 
 
     '''
@@ -40,11 +63,9 @@ def main(genomes, config):
     paddleC.rect.x = 670
     paddleC.rect.y = 200
     '''
-    all_sprites_list = pygame.sprite.Group()
+   
 
     all_sprites_list.add(wall)
-    all_sprites_list.add(paddleB)
-    all_sprites_list.add(ball)
     #all_sprites_list.add(paddleC)
     #more than one paddle per side works
     #loop for main game not just single player
@@ -57,7 +78,7 @@ def main(genomes, config):
     scoreB = 0
 
     #main loop
-    while mainLoop == True:
+    while mainLoop == True and len(paddles) != 0:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 mainLoop = False
@@ -65,8 +86,14 @@ def main(genomes, config):
                     if event.key==pygame.K_x: #Pressing the x Key will quit the game
                          carryOn=False
      
-        #Moving the paddles when the user uses the arrow keys (player A) or "W/S" keys (player B) 
+        #Moving the paddles when the user uses the arrow keys (player A) or "W/S" keys (player B)
+       
+        #increase paddle fitness each second or so
+        
+           
+       
         keys = pygame.key.get_pressed()
+       
         '''
         if keys[pygame.K_w]:
             wall.moveUp(5)
@@ -80,38 +107,58 @@ def main(genomes, config):
         all_sprites_list.update()
         '''
         #Check if the ball is bouncing against any of the 4 walls:
-        if ball.rect.x>=690:
-            scoreA+=1
-            ball.rect.x = 345
-            ball.rect.y = 195
-            ball.velocity[0] = -ball.velocity[0]
-        if ball.rect.x<=0:
-            scoreB+=1
-            ball.rect.x = 345
-            ball.rect.y = 195
-            ball.velocity[0] = -ball.velocity[0]
-        if ball.rect.y>490:
-            ball.velocity[1] = -ball.velocity[1]
-        if ball.rect.y<0:
-            ball.velocity[1] = -ball.velocity[1]
+        for ball in balls:
+            if ball.rect.x>=690:
+                ball.rect.x = 345
+                ball.rect.y = 195
+                ball.velocity[0] = -ball.velocity[0]
+            if ball.rect.x<=0:
+                ball.rect.x = 345
+                ball.rect.y = 195
+                ball.velocity[0] = -ball.velocity[0]
+            if ball.rect.y>490:
+                ball.velocity[1] = -ball.velocity[1]
+            if ball.rect.y<0:
+                ball.velocity[1] = -ball.velocity[1]
+            
 
-        if pygame.sprite.collide_mask(ball, wall) or pygame.sprite.collide_mask(ball, paddleB):
-          ball.bounce()
-
+        
+        for paddle in paddles:
+            if paddle.rect.x < ball.rect.x:
+                counter[paddles.index(paddle)] += 1
+                ge[paddles.index(paddle)].fitness += 5
+               
+                if counter[paddles.index(paddle)] == 5:
+                    paddles.pop(paddles.index(paddle))
+                    ge.pop(paddles.index(paddle))
+                    nets.pop(paddles.index(paddle))
+                    '''
+            if pygame.sprite.collide_mask(ball, wall) or pygame.sprite.collide_mask(ball, paddle):
+                ball.bounce()
+               
+'''
+        for x, paddle in enumerate(paddles):
+            ge[x].fitness += 0.1
+           
+            output = nets[paddles.index(paddle)].activate((paddle.rect.y, balls[x].rect.x, balls[x].rect.y))
+            if output[0] > 0.5:
+                paddle.moveUp(1)
+            if output[1] > 0.5:
+                paddle.moveDown(1)
 
         #drawing things goes here
         screen.fill(black)
         pygame.draw.line(screen, white, [349, 0], [349, 500], 5)
         #draw them sprites
         all_sprites_list.draw(screen)
-
+        
         #Display scores:
         font = pygame.font.Font(None, 74)
         text = font.render(str(scoreA), 1, white)
         screen.blit(text, (250,10))
         text = font.render(str(scoreB), 1, white)
         screen.blit(text, (420,10))
-        
+       
         #update screen
         pygame.display.flip()
 
@@ -119,24 +166,25 @@ def main(genomes, config):
         clock.tick(60)
 
     pygame.quit()
-main()
+
 
 #loads in config file and such
 def run(config_path):
     config = neat.config.Config(neat.DefaultGenome, neat.DefaultReproduction,
                          neat.DefaultSpeciesSet, neat.DefaultStagnation,
-                         config_file)
-    p = neat.Population(Config)
+                         config_path)
+    p = neat.Population(config)
     #gives info about gen
     p.add_reporter(neat.StdOutReporter(True))
     stats = neat.StatisticsReporter()
     p.add_reporter(stats)
 
     winner = p.run(main, 50)
+   
+    print('\nBest genome:\n{!s}'.format(winner))
 
 if __name__ == "__main__":
     local_dir = os.path.dirname(__file__)
     config_path = os.path.join(local_dir, "config-feedforward.txt")
     run(config_path)
-
 
