3078538ecf4a8240bc2218e570c5f1f43a66550f
penneShells <61021287+penneShells@users.noreply.github.com>
2022-09-13 14:36:31 +0100

    Update pong_ai.py
    this is some of the most convoluted and beautiful code ive ever written if it doesnt work then there is no god

--- pongthing/pong_ai.py
+++ pongthing/pong_ai.py
@@ -17,14 +17,42 @@ size = (700, 500)
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption("Pong Time")
 
+paddleA = Paddle(white, 10, 10000000)
+paddleA.rect.x = 20
+paddleA.rect.y = 0
+ '''
+paddleB = Paddle(white, 10, 100)
+paddleB.rect.x = 670
+paddleB.rect.y = 200'''
+
+ball = Ball(white,10,10)
+ball.rect.x = 345
+ball.rect.y = 195
+
+
+all_sprites_list = pygame.sprite.Group()
+
+all_sprites_list.add(paddleA)
+
+all_sprites_list.add(ball)
+#all_sprites_list.add(paddleC)
+#more than one paddle per side works
+#loop for main game not just single player
+mainLoop = True
+
+#basically just fps control
+clock = pygame.time.Clock()
+
+scoreA = 0
+scoreB = 0
 
 def main(genomes, config):
-
+    
     paddles = []
-    nets = []
     ge = []
-    all_sprites_list = pygame.sprite.Group()
+    nets = []
     counter = []
+    
     for genome_id, g in genomes:
         g.fitness = 0
         net = neat.nn.FeedForwardNetwork.create(g, config)
@@ -38,40 +66,8 @@ def main(genomes, config):
         all_sprites_list.add(paddle)
         count = 0
         counter.append(count)
-
-   
-    wall = Paddle(white, 10, 10000)
-    wall.rect.x = 20
-    wall.rect.y = 0
-    '''
-    paddleB = Paddle(white, 10, 100)
-    paddleB.rect.x = 670
-    paddleB.rect.y = 200
-'''
-    ball = Ball(white,10,10)
-    ball.rect.x = 345
-    ball.rect.y = 195
-
-    '''
-    paddleC = Paddle(white, 10, 100)
-    paddleC.rect.x = 670
-    paddleC.rect.y = 200
-    '''
-   
-
-    all_sprites_list.add(wall)
-    all_sprittes_list.add(ball)
-    #all_sprites_list.add(paddleC)
-    #more than one paddle per side works
-    #loop for main game not just single player
-    mainLoop = True
-
-    #basically just fps control
-    clock = pygame.time.Clock()
-
-    scoreA = 0
-    scoreB = 0
-
+    
+    
     #main loop
     while mainLoop == True and len(paddles) != 0:
         for event in pygame.event.get():
@@ -81,32 +77,29 @@ def main(genomes, config):
                     if event.key==pygame.K_x: #Pressing the x Key will quit the game
                          carryOn=False
      
-        #Moving the paddles when the user uses the arrow keys (player A) or "W/S" keys (player B)
-       
-        #increase paddle fitness each second or so
-       
-           
-       
-        keys = pygame.key.get_pressed()
-       
+        #Moving the paddles when the user uses the arrow keys (player A) or "W/S" keys (player B) 
         '''
+        keys = pygame.key.get_pressed()
         if keys[pygame.K_w]:
-            wall.moveUp(5)
+            paddleA.moveUp(5)
         if keys[pygame.K_s]:
-            wall.moveDown(5)
+            paddleA.moveDown(5)
         if keys[pygame.K_UP]:
             paddleB.moveUp(5)
         if keys[pygame.K_DOWN]:
             paddleB.moveDown(5)  
-        #game logic goes here
-        all_sprites_list.update()
         '''
+        #game logic goes here
+            
+        all_sprites_list.update()
         #Check if the ball is bouncing against any of the 4 walls:
+        
         if ball.rect.x>=690:
             ball.rect.x = 345
             ball.rect.y = 195
             ball.velocity[0] = -ball.velocity[0]
         if ball.rect.x<=0:
+            scoreB+=1
             ball.rect.x = 345
             ball.rect.y = 195
             ball.velocity[0] = -ball.velocity[0]
@@ -114,14 +107,10 @@ def main(genomes, config):
             ball.velocity[1] = -ball.velocity[1]
         if ball.rect.y<0:
             ball.velocity[1] = -ball.velocity[1]
-           
-
-       
+        
         for paddle in paddles:
-            if paddle.rect.x < ball.rect.x:
- 
             #this might resolve those fucking issues so remember what this does and also dont submit this homework with this comment still here itll look bad
-            if pygame.sprite.collide_mask(ball, wall):
+            if pygame.sprite.collide_mask(ball, paddleA):
                 ball.bounce()
                
             if pygame.sprite.collide_mask(ball, paddle):
@@ -137,12 +126,10 @@ def main(genomes, config):
                         paddles.pop(paddles.index(paddle))
                         ge.pop(paddles.index(paddle))
                         nets.pop(paddles.index(paddle))
-                       
-                   
-                   
                 ball.bounce()
                
 
+        #output input ai stuff
         for x, paddle in enumerate(paddles):
             ge[x].fitness += 0.1
            
@@ -157,14 +144,14 @@ def main(genomes, config):
         pygame.draw.line(screen, white, [349, 0], [349, 500], 5)
         #draw them sprites
         all_sprites_list.draw(screen)
-       
+
         #Display scores:
         font = pygame.font.Font(None, 74)
         text = font.render(str(scoreA), 1, white)
         screen.blit(text, (250,10))
         text = font.render(str(scoreB), 1, white)
         screen.blit(text, (420,10))
-       
+        
         #update screen
         pygame.display.flip()
 
@@ -173,8 +160,6 @@ def main(genomes, config):
 
     pygame.quit()
 
-
-#loads in config file and such
 def run(config_path):
     config = neat.config.Config(neat.DefaultGenome, neat.DefaultReproduction,
                          neat.DefaultSpeciesSet, neat.DefaultStagnation,
@@ -193,3 +178,4 @@ if __name__ == "__main__":
     local_dir = os.path.dirname(__file__)
     config_path = os.path.join(local_dir, "config-feedforward.txt")
     run(config_path)
+
